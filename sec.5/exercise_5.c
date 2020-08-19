a02d0c0f562d03b2f9c389eaaf2343a2142dfa1a
Damian Bigajczyk <d.bigajczyk@gmail.com>
2020-08-19 06:25:03 +0200

    Add exercise

--- sec.5/exercise_5.c
+++ sec.5/exercise_5.c
@@ -30,6 +30,7 @@ static int getLine(char *line, size_t n);
 static char *alloc(size_t n);
 static void afree(char *p);
 static void swap(char *v[], int i, int j);
+static void swap_2(void *v[], int i, int j);
 
 int getint(int *pn)
 {
@@ -224,6 +225,15 @@ void writelines(char *lineptr[], int nlines)
 {
 	while (nlines-- > 0)
 		printf("%s\n", *lineptr++);
+}
+void writelines_2(char *lineptr[], int nlines, int dec)
+{
+	if (dec)
+		while (nlines--)
+			printf("%s\n", lineptr[nlines]);
+	else
+		while (nlines--)
+			printf("%s\n", *lineptr++);
 }
 void qsort(char *v[], int left, int right)
 {
@@ -492,6 +502,50 @@ void esettab(int argc, char **argv, char *s)
 	}
 	
 }
+void qsort_2(void *v[], int left, int right, int (*comp)(void *, void *))
+{
+	int i, last;
+	
+	if (left >= right)
+		return;
+	swap_2(v, left, (left + right)/2);
+	last = left;
+	for (i = left+1; i <= right; i++)
+		if ((*comp)(v[i], v[left]) < 0)
+			swap_2(v, ++last, i);
+	swap_2(v, left, last);
+	qsort_2(v, left, last-1, comp);
+	qsort_2(v, last+1, right, comp);
+
+}
+int numcmp(char *s1, char *s2)
+{
+	double v1, v2;
+
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	else
+		return 0;
+}
+int strcmp_2(char *str1, char *str2)
+{
+	while ((*str1 != '\0' && *str2 != '\0') && *str1 == *str2) {
+		str1++;
+		str2++;
+	}
+	return (*str1 == *str2) ? 0 : *str1 - *str2;
+}
+int charcmp(char *s1, char *s2)
+{
+	for ( ; tolower(*s1) == tolower(*s2); s1++, s2++)
+		if (*s1 == '\0')
+			return 0;
+	return tolower(*s1) - tolower(*s2);
+}
 
 /***************** Static functions ****************/
 
@@ -535,6 +589,14 @@ static void afree(char *p)
 static void swap(char *v[], int i, int j)
 {
 	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+static void swap_2(void *v[], int i, int j)
+{
+	void *temp;
 
 	temp = v[i];
 	v[i] = v[j];
--- sec.5/exercise_5.h
+++ sec.5/exercise_5.h
@@ -10,33 +10,44 @@
 
 int getint(int *pn);
 int getfloat(float *pn);
+int getop(char *s);
+double atof(char *s);
+int atoi_2(char *s);
+
+char *reverse(char *s);
 char *strcat(char *dest, const char *src);
+char *strncat(char *dest, const char *src, size_t n);
+char *strncpy(char *dest, const char *src, size_t n);
 int strend(const char *str1, const char *str2);
 int strend_ver2(const char *s, const char *t);
-char *strncpy(char *dest, const char *src, size_t n);
-char *strncat(char *dest, const char *src, size_t n);
+int strcmp_2(char *str1, char *str2);
 int strncmp(const char *str1, const char *str2, size_t n);
-char *reverse(char *s);
 int strindex(char *s, char *t);
+int charcmp(char *s1, char *s2);
+
 int readlines(char *lineptr[], int nlines);
-void clearHeap(char *lineptr[], int nlines);
-void writelines(char *lineptr[], int nlines);
-void qsort(char *lineptr[], int left, int right);
 int readlines_ver2(char *lineptr[], char *linebuf, int nlines);
+void writelines(char *lineptr[], int nlines);
+void writelines_2(char *lineptr[], int nlines, int dec);
+void clearHeap(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+void qsort_2(void *lineptr[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
+
 int day_of_year(__uint16_t year, __uint8_t month, __uint16_t day);
 void month_day(__uint16_t year, __uint16_t yearday, __uint8_t *pmonth, __uint8_t *pday);
 int day_of_year_ver2(int year, int month, int day);
 void month_day_ver2(int year, int yearday, int *pmonth, int *pday);
-int getop(char *s);
-double atof(char *s);
+
 void ungets(char *s);
 void push(double number);
 double pop(void);
+
 void settab(int argc, char **argv, char *s);
+void esettab(int argc, char **argv, char *s);
 void entab(char *s);
 void detab(char *s);
 int tabpos(int pos, char *s);
-int atoi_2(char *s);
-void esettab(int argc, char **argv, char *s);
 
 #endif
